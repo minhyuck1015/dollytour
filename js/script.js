c0fd65817cb6a49e2d961774742e0fb00866ef22
minhyuck1015 <103724127+minhyuck1015@users.noreply.github.com>
2022-04-21 17:44:33 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(function(){
 	AOS.init();	
 });
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100                                                          ) {
+        $('.top').fadeIn();
+    } else {
+        $('.top').fadeOut();
+    }
+	var height = $(window).scrollTop();
+    if (height > 100                                                          ) {
+        $('.booking').fadeIn();
+    } else {
+        $('.booking').fadeOut();
+    }
+});
